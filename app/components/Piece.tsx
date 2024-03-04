80518f6c1d61010a919e6169adb71556502cb08e
Juan Manuel Garcia <jmg.utn@gmail.com>
2024-03-04 15:19:33 -0300

    fix mobile

--- app/components/Piece.tsx
+++ app/components/Piece.tsx
@@ -5,19 +5,18 @@ const RoundedPiece = ({ color, row }: { color: string, row: number }) => {
 
     return <>
         <Piece color={color} row={row} size={50} className="sm:hidden"/>
-        <Piece color={color} row={row} size={100} className="sm:visible"/>
+        <Piece color={color} row={row} size={100} className="sm:visible hidden"/>
     </>
 }
 
 const Piece = ({ color, row, size, className }: { color: string, row: number, size: number, className: string }) => {
 
-    return (<motion.div className={className} initial={{ y: - (row + 1) * size }} animate={{ y: 0 }} transition={{ ease: "easeOut", duration: 0.5 }}>
-            <div style={{
-                backgroundColor: color,
-            }} className="border-2 dark:border-white border-gray-300 rounded-full piece">
-            </div>
-        </motion.div>
-    )
+    return <motion.div className={className} initial={{ y: - (row + 1) * size }} animate={{ y: 0 }} transition={{ ease: "easeOut", duration: 0.5 }}>
+        <div style={{
+            backgroundColor: color,
+        }} className="border-2 dark:border-white border-gray-300 rounded-full piece">
+        </div>
+    </motion.div>
 }
 
 const Tile = ({children}: {children: React.ReactNode}) => {
